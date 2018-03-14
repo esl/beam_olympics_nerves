da2cbd5d6c1eea58b4de125b8674d45c30fea412
Felipe Ripoll <ferigis@gmail.com>
2018-03-14 09:18:32 -0600

    SF code beam 2018 set up

--- beam_olympics_leaderboard/config/config.exs
+++ beam_olympics_leaderboard/config/config.exs
@@ -23,7 +23,7 @@ config :logger, :console,
 import_config "#{Mix.env}.exs"
 
 config :beam_olympics_leaderboard,
-  server: :"olympics@192.168.0.100",
+  server: :"sfcodebeamchallenge2018@10.0.252.49",
   s3_bucket: System.get_env("AWS_S3_BUCKET")
 
 config :ex_aws,
